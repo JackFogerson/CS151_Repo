939a06b51d58ecc200f05ffd6d8e33cf358cbdc2
JackFogerson <40747086+JackFogerson@users.noreply.github.com>
2019-08-25 00:41:33 -0700

    Create Strings.java

---
+++ hw1/Strings.java
@@ -0,0 +1,17 @@
+public class Strings{
+	public static String uniqueLetters(String str){
+		String result = "";
+		for(int i=0; i<str.length(); i++){
+		boolean match = false;
+			for(int j=0; j<str.length(); j++){
+				if(i!=j&&str.charAt(i)==str.charAt(j)){
+					match = true;
+				}
+			}
+			if(match!=true){
+				result = result+(str.charAt(i));
+			}
+		}
+		return result;
+	}
+}
