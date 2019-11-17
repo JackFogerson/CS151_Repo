86944e82be53ee37238056c91b6b0e658cce42cd
JackFogerson <40747086+JackFogerson@users.noreply.github.com>
2019-11-17 13:23:24 -0800

    Create hw4pt1.java

---
+++ HW4/Q 1.A/hw4pt1.java
@@ -0,0 +1,34 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class hw4pt1 {
+
+	public static Runnable deposit(Account bankAccount, double depAmount,int repetitions) {
+		   return () ->
+		      {
+		         for (int i = 0; i < repetitions; i++)
+		            bankAccount.makeDeposit(depAmount);
+		      };
+	}
+	
+	public static Runnable withdrawl(Account bankAccount, double withAmount,int repetitions) {
+		   return () ->
+		      {
+		         for (int i = 0; i < repetitions; i++)
+		            bankAccount.makeWithdrawl(withAmount);
+		      };
+	}
+	
+	public static void main(String[] args) {
+		Account bankAccount = new Account(50);
+		int repetitions = 500;
+		
+		ExecutorService service = Executors.newCachedThreadPool();
+		Runnable r1 = deposit(bankAccount,25,repetitions);
+		Runnable r2 = withdrawl(bankAccount,25,repetitions);
+		service.execute(r1);
+		service.execute(r2);
+		service.shutdown();
+	}
+	
+}
